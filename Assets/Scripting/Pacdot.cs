b7ab71334567de9c86b9ad67f8e382fd4b7c7e12
FelixLiman <felixlim75@gmail.com>
2021-01-20 15:40:29 +0700

    just pac-man

---
+++ Assets/Scripting/Pacdot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    public TMPro.TMP_Text scoreLbl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D co) {
+        if (co.name == "Picture2_0") 
+            scoreLbl.text = "3";
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripting/PacmanMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour
+{
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dest = transform.position;    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            dest = (Vector2)transform.position + Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            dest = (Vector2)transform.position + Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            dest = (Vector2)transform.position - Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            dest = (Vector2)transform.position - Vector2.down;
+        }
+        Vector2 dir = dest - (Vector2)transform.position; 
+        GetComponent<Animator>().SetFloat("DirX", dir.x); 
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+}
